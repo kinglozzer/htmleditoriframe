602790b23cb012849522cbc095afd99a456036fc
helpfulrobot <chris@silverstripe.com>
2015-12-18 09:03:16 +1300

    Converted to PSR-2

--- code/HtmlEditorField_IframeEmbed.php
+++ code/HtmlEditorField_IframeEmbed.php
@@ -1,44 +1,53 @@
 <?php
 
-class HtmlEditorField_IframeEmbed extends HtmlEditorField_File {
+class HtmlEditorField_IframeEmbed extends HtmlEditorField_File
+{
 
-	protected $iframe;
+    protected $iframe;
 
-	public function __construct($url, $file = null) {
-		parent::__construct($url, $file);
-		$this->iframe = $url;
-	}
+    public function __construct($url, $file = null)
+    {
+        parent::__construct($url, $file);
+        $this->iframe = $url;
+    }
 
-	public function getWidth() {
-		return 500;
-	}
+    public function getWidth()
+    {
+        return 500;
+    }
 
-	public function getHeight() {
-		return 500;
-	}
+    public function getHeight()
+    {
+        return 500;
+    }
 
-	public function getPreview() {
-		return false;
-	}
+    public function getPreview()
+    {
+        return false;
+    }
 
-	public function getName() {
-		return $this->iframe;
-	}
+    public function getName()
+    {
+        return $this->iframe;
+    }
 
-	public function getType() {
-		return 'Iframe';
-	}
+    public function getType()
+    {
+        return 'Iframe';
+    }
 
-	public function getOembed() {
-		return $this->iframe;
-	}
+    public function getOembed()
+    {
+        return $this->iframe;
+    }
 
-	public function appCategory() {
-		return 'iframe';
-	}
-	
-	public function getInfo() {
-		return false;
-	}
-
+    public function appCategory()
+    {
+        return 'iframe';
+    }
+    
+    public function getInfo()
+    {
+        return false;
+    }
 }
